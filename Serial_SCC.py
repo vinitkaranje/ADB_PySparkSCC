f0771ecd4f2bd47a564e1a740a374aa5305d39be
vinitkaranje <35327291+vinitkaranje@users.noreply.github.com>
2018-05-04 16:49:28 -0700

    Add files via upload

---
+++ Serial_SCC.py
@@ -0,0 +1,84 @@
+
+# coding: utf-8
+
+# In[5]:
+from pyspark import SparkConf, SparkContext
+import networkx as nx
+import matplotlib.pyplot as plt
+import itertools
+from itertools import chain
+import numpy as np
+import random
+import time
+
+#G = nx.DiGraph()
+#G.add_edges_from([(1, 0), (0, 4), (2, 1), (4, 1), (5, 4), (5, 1), (5, 6), (6, 5), (2, 3), (3, 2), (7, 7), (7, 3), (7, 6), (6,2)])
+#G = nx.gn_graph(1500)
+#random.seed(1)
+G=nx.read_edgelist("p2p-Gnutella_10000.txt",nodetype=int)
+#pos = nx.spring_layout(G)
+#nx.draw_networkx_nodes(G, pos, node_size = 500)
+#nx.draw_networkx_labels(G, pos)
+#nx.draw_networkx_edges(G, pos,arrows=True)
+#plt.axis('off')
+#plt.show()
+
+
+V = list(G.nodes())
+sc_conf = SparkConf()
+sc_conf.setAppName("SCC_Graph")
+sc_conf.setMaster('local[2]')
+sc_conf.set('spark.executor.memory', '4g')
+sc_conf.set('spark.executor.cores', '10')
+sc_conf.set('spark.cores.max', '40')
+sc_conf.set('spark.logConf', True)
+def Desc(G,v):
+    nodes = list(G.nodes())
+    Desc_v = list()
+    for a in nodes:
+        try:
+            temp = list(nx.all_shortest_paths(G, source=v, target=a))
+            temp = list(chain.from_iterable(temp))
+            if temp:
+                Desc_v.append(temp[-1])
+        except nx.exception.NetworkXNoPath:
+            continue
+    return Desc_v
+
+def Pred(G,v):
+    nodes = list(G.nodes())
+    Pred_v = list()
+    for a in nodes:
+        try:
+            temp = list(nx.all_shortest_paths(G, source=a, target=v))
+            temp = list(chain.from_iterable(temp))
+            if temp:
+                Pred_v.append(temp[0])
+        except nx.exception.NetworkXNoPath:
+            continue
+    return Pred_v
+TotalSCC = list()
+def DCSC(V):
+    SCC = list()
+    if V:
+        v = random.choice(V)
+        Pred_v = Pred(G,v)
+        Desc_v = Desc(G,v)
+        SCC = (np.intersect1d(Pred(G,v), Desc(G,v))).tolist()
+        if SCC not in TotalSCC:
+            TotalSCC.append(SCC)
+        Rem_v = list(set(V).difference(set(np.unique(np.concatenate((Desc_v,Pred_v),0)))))
+        
+        V = list(set(V).difference(set(list(chain.from_iterable(TotalSCC)))))
+        
+        DCSC(list(set(Pred_v).difference(set((list(chain.from_iterable(TotalSCC)))))))
+        DCSC(list(set(Desc_v).difference(set((list(chain.from_iterable(TotalSCC)))))))
+        DCSC(Rem_v)
+    else:
+        return
+start_time = time.time()
+DCSC(V)
+print("Strongly connected components in this graph are: ",TotalSCC)
+print("Number of SCC in graph are: ",len(TotalSCC))
+print("--- %s seconds ---" % (time.time() - start_time))
+
